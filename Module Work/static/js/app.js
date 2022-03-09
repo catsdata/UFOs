592c351bdae8b7ff428137a8c1fc860de962ff37
catsdata <96197979+catsdata@users.noreply.github.com>
2022-03-09 14:42:48 -0500

    module update 11.5.4

--- Module Work/static/js/app.js
+++ Module Work/static/js/app.js
@@ -19,3 +19,24 @@ function buildTable(data) {
       );
     });
   }
+// create filter by date
+  function handleClick() {
+    // look for #datetime id in the HTML tags
+    let date = d3.select("#datetime").property("value");
+    // state filter the data from imported data.js file from above
+    let filteredData = tableData;
+    // if-statement; Check if date was entered and filter the data using that date.
+    if (date) {
+    // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
+
+    // Rebuild the table using the filtered data; If no date entered, then original tableData.
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
